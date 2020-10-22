67a1237e7a060ab542c2d673c825672b8fda780c
PinaColada928 <32275347+PinaColada928@users.noreply.github.com>
2020-10-22 13:55:37 -0700

    Added login html and student ID attribute

--- login/models.py
+++ login/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Users(models.Model):
     username    = models.CharField(max_length=60)
-    user        = models.EmailField(max_length=254) # This is the email 
+    user        = models.EmailField(max_length=254) # This is the email
+    studentID   = models.CharField(max_length=10)
     password    = models.CharField(max_length=50)
     createdDate = models.DateTimeField(auto_now=False, auto_now_add=True)
     lastAccessDate  = models.DateTimeField(auto_now=False, auto_now_add=False)
