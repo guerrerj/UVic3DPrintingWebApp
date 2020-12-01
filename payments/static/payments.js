9e387d604e14e083d916d283d9c44627f6e7965a
= <jose.guerrero10@yahoo.com>
2020-11-30 19:06:01 -0800

    sm27

--- payments/static/payments.js
+++ payments/static/payments.js
@@ -13,6 +13,29 @@ fetch(baseurl + "/payments/config/")
       })
   });
 
+  const nav = document.querySelector('.mainNavbar');
+  const header = document.querySelector('.js-nav');
+  const navHeight = nav.getBoundingClientRect().height;
+
+  const stickyNav = function (entries){
+    const [entry] = entries;
+    if (!entry.isIntersecting) {
+      nav.classList.add('sticky');
+    } 
+    else{
+      nav.classList.remove('sticky');
+    } 
+  };
+
+  const headerObserver = new IntersectionObserver(
+    stickyNav,{
+      root:null,
+      threshold: 0, // percentage we want visible in the view port with the current target 
+    }
+  );
+  headerObserver.observe(header); 
+
+
   function onClickEvent(e){
     try{
         // Get Checkout Session ID
